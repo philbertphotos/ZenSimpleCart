039661e34e5a5555e5a370472c1397e33b75a6ce
philbertphotos <joe@philbertphotos.com>
2013-02-24 22:48:27 -0500

    Update ZenSimpleCart.php
    Major updated 
    1. Added a light and dark css files (dark is todo)
    2. Flat Shipping Rate (more shipping options to come)
    3. Updated the js files for more functionality
    4. added description option that shows up in Paypal. 
    5. TONS of bug fixes and small changes.

--- ZenSimpleCart.php
+++ ZenSimpleCart.php
@@ -1,23 +1,21 @@
 <?php
-/*
- * Simple shopping cart for Zenphoto CMS 
+/**
+ * 
+ * Place <?php if (function_exists('printCartPrice')) { ?><?php printCartPrice(); ?><?php  } ?> in your theme's image.php file where you want it to appear.
+ 
+ 
+ *	Place in the index or header <?php if (function_exists('printCartWidget')) { ?><?php printCartWidget(); ?><?php  } ?>
+ *
+ * 
+  *a very simple but smart shopping cart  
  * @package plugins
- * (c) Joseph Philbert
- *
- * @package zenphoto.plugin.zensimplecart
- * Place the "code" in your theme's image.php file where you want it to appear.
- * <?php if (function_exists('printCartPrice')) { ?><?php printCartPrice(); ?><?php  } ?>
- *
- * Place in the themes index.php,inc_header.php, inc_menu.php, etc where you want your cart widget to display.
- * <?php if (function_exists('printCartWidget')) { ?><?php printCartWidget(); ?><?php  } ?>
  */
-
 
 $plugin_is_filter = 2;
 $plugin_description = gettext("Adds SimpleCart fuctions to ZenPhoto.");
 
 $plugin_author = "Joseph Philbert";
-$plugin_version = '0.1';
+$plugin_version = '1.3';
 $plugin_URL = 'http://www.philbertphotography.com/';
 $option_interface = 'ZenSimpleCartOptions';
 
@@ -38,30 +36,47 @@ zp_register_filter('theme_head','ZenSimpleCartHead');
 class ZenSimpleCartOptions {
 
 	function ZenSimpleCartOptions() {
-		setOptionDefault('zensimplecart_email', '');
+		setOptionDefault('zensimplecart_email', 'none@none.com');
 		setOptionDefault('zensimplecart_checkout', '');
 		setOptionDefault('zensimplecart_currency', 'USD');
+		setOptionDefault('zensimplecart_css', 'lightcart.css');
 	}
 
 	function getOptionsSupported() {
-		return array(	
-								
+		return array(										
 	gettext('Paypal Email ') => array('key' => 'zensimplecart_email', 'type' => OPTION_TYPE_TEXTBOX,
-										'desc' => gettext("Enter your Paypal email(amazon and googlecheckout do not work")),								
+										'desc' => gettext("Enter your paypal email address.")),	
+										
 	gettext('Checkout Type ID') => array('key' => 'zensimplecart_checkout', 'type' => OPTION_TYPE_SELECTOR,
 										'order'=>0,
-										'selections' => array(gettext('PayPal') => 'PayPal', gettext('Google Wallet') => 'GoogleCheckout',gettext('Amazon Payments') => 'AmazonPayments'),
+										'selections' => array(gettext('PayPal') => 'PayPal', gettext('GoogleCheckout') => 'GoogleCheckout',gettext('Amazon Payments') => 'AmazonPayments'),
 										'desc' => gettext('Choose a payment type.')),
 										
+	gettext('Style') => array('key' => 'zensimplecart_css', 'type' => OPTION_TYPE_SELECTOR, 
+				'order' => 1,
+				'selections' => array(gettext('dark') => 'darkcart.css', gettext('light') => 'lightcart.css'),
+				'desc' => gettext('Select a dark or light overall color style for your cart')),
+				
+	gettext(' Flat Shipping Rate') => array('key' => 'zensimplecart_flatship', 'type' => OPTION_TYPE_TEXTBOX,
+										'desc' => gettext("Enter your flatshiping rate")),
 										
-   gettext('Currency') => array('key' => 'zensimplecart_currency', 'type' => OPTION_TYPE_SELECTOR,
+	gettext('Currency') => array('key' => 'zensimplecart_currency', 'type' => OPTION_TYPE_SELECTOR,
 			'order'=>0,
-			'selections' => array(gettext('US Dollar') => 'USD', gettext('British Pound Dollar') => 'GBP',gettext('Australian Dollar') => 'AUD', gettext('Brazilian Real') => 'BRL', gettext('Canadian Dollar') => 'CAD',gettext('Czech Koruna') => 'CZK',gettext('Danish Krone') => 'DKK',gettext('Euro') => 'EUR',gettext('Hong Kong Dollar') => 'HKD',gettext('Hungarian Forint') => 'HUF',gettext('Japanese Yen') => 'JPY',gettext('Mexican Peso') => 'MXN',gettext('Swiss Franc') => 'CHF'),
+			'selections' => array(gettext('US Dollar') => 'USD', gettext('Australian Dollar') => 'AUD', gettext('Brazilian Real') => 'BRL', gettext('Canadian Dollar') => 'CAD',gettext('Czech Koruna') => 'CZK',gettext('Danish Krone') => 'DKK',gettext('Euro') => 'EUR',gettext('Hong Kong Dollar') => 'HKD',gettext('Hungarian Forint') => 'HUF',gettext('Japanese Yen') => 'JPY',gettext('Mexican Peso') => 'MXN',gettext('Swiss Franc') => 'CHF'),
 	        'desc' => gettext('Select the currency type')),
 		
 		);
 	}
 }
+
+
+
+
+/*shippingFlatRate, 
+shippingQuantityRate, 
+shippingTotalRate.  
+shippingCustom*/
+
 
 /*function getNewFileName($filename, $dir) {
    if (is_file("$dir/$filename")) {
@@ -123,9 +138,10 @@ foreach ($codeblock as $value) {
 $pricelist = explode("|", $value);
 echo '<div class="zensimpleprice">';
 echo "<div class='simpleCart_shelfItem'>";
-echo "<h2 class='item_name'>".$file_name."</h2>";
-//echo "<h2 class='item_description'>testing/8x10/wtf</h2>"; //paper type TODO
-echo "<div class='selectitems'><h1 id='priceshow'>$0</h1>";
+echo "<div class='item_name' style='display:none'>".$file_name."</div>";
+echo "<div id='pricedesc' class='item_description'style='display:none'>test</div>"; //DESCRIPTION TODO
+echo "<div><h1 id='priceshow'>$0</h1></div>"; //Show price TODO add option pricing
+echo "<div class='selectitems'>";
 	//create selection values
 echo '<select id="priceselect" class="item_price">';
 echo "<option value='0'>choose option</option>";
@@ -133,29 +149,30 @@ for ($i = 0; $i < count(getPriceList($value)['price']); ++$i) {
 echo "<option value='".getPriceList($value)['price'][$i]."'>";
 echo getPriceList($value)['desc'][$i]."</option>";
 }
-echo "</select> <br /> </div>";
-    
+echo "</select> <br /> </div>";    
 echo "<div class='item_thumb'>".printCustomSizedImage(null, 150, 150)."</div>";
-echo "<p><input id='' type='button' class='item_add addProduct' value='Add to cart' /></p>";
+echo "<p><input type='button' class='item_add button button_accent addProduct' value='Add to cart' /></p>";
 echo "</div>";
 echo "</div>";
 /*   Array Test 
 $json = json_encode($datadesc);
     $phpStringArray = str_replace(array("{","}",":"), array("array(","}","=>"), $json);
-    echo $phpStringArray;*/             
+    echo $phpStringArray;*/           
 } 
 }
 } 	
+
 function ZenSimpleCartHead() { 
 ?>
 	<script type="text/javascript" src="<?php echo WEBPATH.'/'.USER_PLUGIN_FOLDER; ?>/ZenSimpleCart/js/simpleCart.min.js"></script>
-		<script type="text/javascript" src="<?php echo WEBPATH.'/'.USER_PLUGIN_FOLDER; ?>/ZenSimpleCart/js/plugins.js" defer=""></script>
-		
-				<script type="text/javascript" src="<?php echo WEBPATH.'/'.USER_PLUGIN_FOLDER; ?>/ZenSimpleCart/js/custom.js" defer=""></script>
+	<script type="text/javascript" src="<?php echo WEBPATH.'/'.USER_PLUGIN_FOLDER; ?>/ZenSimpleCart/js/plugins.js" defer=""></script>
+	<script type="text/javascript" src="<?php echo WEBPATH.'/'.USER_PLUGIN_FOLDER; ?>/ZenSimpleCart/js/custom.js" defer=""></script>
 	<link rel="stylesheet" href="<?php echo WEBPATH.'/'.USER_PLUGIN_FOLDER; ?>/ZenSimpleCart/css/zensimplecart.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo WEBPATH.'/'.USER_PLUGIN_FOLDER; ?>/ZenSimpleCart/css/<?php echo getOption('zensimplecart_css'); ?>" type="text/css" />
 	    <script>
 		
-    simpleCart({	
+    simpleCart({
+	shippingFlatRate: "<?php echo getOption('zensimplecart_flatship'); ?>",	
 	cartColumns: [
     { attr: "image", label: false, view: "Image"},
     { view: "image" , attr: "thumb", label: false },
@@ -170,7 +187,7 @@ function ZenSimpleCartHead() {
     checkout: {
     type: "<?php echo getOption('zensimplecart_checkout'); ?>",
     email: "<?php echo getOption('zensimplecart_email'); ?>",
-	email: "<?php echo getOption('zensimplecart_currency'); ?>"
+	currency: "<?php echo getOption('zensimplecart_currency'); ?>",
     },
     });
 	
@@ -205,33 +222,38 @@ function ZenSimpleCartHead() {
 
 function printCartWidget() { ?>
 <!--Start printCartWidget -->
-<div id="basket" class="zensimplecart" style="display:none">
-<div id="outercart">
-<div id="cart" class="">
-<div class="cart-total">
-		<a class="tooltip_l" title="Shopping Cart" href="#"> 
-		<span class="simpleCart_quantity">0</span>
-		<span id="cart-total"> </span> item(s) - <i class="simpleCart_total"></i></a>
-	</div>
-	<div class="details">		
-		<div class="arrow"></div>		
-		<div class="cartbasket">
-<div class="simpleCart_items"><h1> Your shopping cart is empty!</h1>
+ <div id="cart">
+  <div class="heading">
+	<a id="basket" title="Shopping Cart" href="#"> 
+	<span>
+    <i></i><i class="simpleCart_quantity"> </i><i>-items Shopping Cart</i></span></a></div>
+  <div class="content shadow">
+       <div class="mini-cart-info">
+	   
+<?php 		
+$scitems = '<script type="text/javascript">simpleCart.quantity()</script>';
+echo $scitems;
+if ( $scitems == 0 ) { ?>
+<div class="simpleCart_items"><h1> Your shopping cart is empty!</h1></div>
+<?php } else { ?>
+<div class="simpleCart_items">
 <div class="item-custom"></div>
 <div class="item-name"></div>
 <div class="item-total"></div>
 </div>
-<div class="clearfix overbtn">
-<a href="javascript:;" class="simpleCart_empty btn left">Empty Cart</a>
-<a class="simpleCart_checkout btn primary right" href="javascript:;">Checkout</a>
+<?php } ?>
+    </div>
+<div class="mini-cart-total">
+<div class="iconcart"> </div>
+	<div align="right"><b>Postage:</b><span class="simpleCart_shipping">0</span></div></br>
+    <div align="right"><b>Sub-Total:</b><span class="simpleCart_total"> </span></div></br>
+    <div align="right"><b>GrandTotal:</b><span class="simpleCart_grandTotal"> </span></div>	
 </div>
-
-<a id="closecart" class="close" onclick="$('#cart').removeClass('active');" title="Close">Ч</a>
-<!--<a onclick="$('#cart').removeClass('active');" title="Close" class="close" id="closecart">Ч</a>-->
+<div class="checkout"><a href="javascript:;" class="simpleCart_empty button button_accent">Empty Cart</a>
+<a class="simpleCart_checkout button" href="javascript:;">Checkout</a>   </div>
+    <div class="cart-arrow"></div>
+ 
 </div>
-	</div>
-	</div>
-	</div>
 </div>
 <!--END printCartWidget -->	 
  <?php } 
